8425948967c6c610d01e0c97eaee47d6e419def7
beeliec <BeeLie.Chai@my.experian.com>
2015-07-26 16:18:22 +0800

    Fixed run_analysis.R bugs

--- run_analysis.R
+++ run_analysis.R
@@ -5,8 +5,8 @@ filename <- "dataset.zip"
 
 ## Download and unzip the dataset:
 if (!file.exists(filename)){
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-  download.file(fileURL, filename, method="curl")
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename )
 }  
 if (!file.exists("UCI HAR Dataset")) { 
   unzip(filename) 
